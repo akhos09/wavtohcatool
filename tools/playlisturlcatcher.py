cb87b04eff7e829f7c3aba0d2ab1843da551aa0f
akhos09 <150979654+akhos09@users.noreply.github.com>
2024-10-24 16:02:01 +0200

    Update playlisturlcatcher.py

--- tools/playlisturlcatcher.py
+++ tools/playlisturlcatcher.py
@@ -4,27 +4,20 @@
 
 from pytube import Playlist
 
-# Replace with the playlist URL you want to extract from
-
 URL_PLAYLIST = input('URL playlist from YouTube: ')
 
-# Initialize the playlist object
 playlist = Playlist(URL_PLAYLIST)
 
-# Prepare to write the output
 output_lines = []
 
-# Iterate through the videos in the playlist
 for idx, video in enumerate(playlist.videos):
     video_url = video.watch_url
     wav_name = f"wav_name{idx + 1}"
     hca_name = f"hca_name{idx + 1}"
     
-    # Prepare the output line
     output_line = f"{video_url},{wav_name},{hca_name}.hca"
     output_lines.append(output_line)
 
-# Save to a file
 with open('playlist_urls.txt', 'w') as f:
     f.write("\n".join(output_lines))
 
